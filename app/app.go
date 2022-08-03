a46d3d76cc530a661fa2ed090099ed3220a0d391
igoritos22 <igoritos_22@hotmail.com>
2022-08-03 16:08:13 -0300

    add funcionalidade de servers

--- app/app.go
+++ app/app.go
@@ -15,17 +15,25 @@ func Gerar() *cli.App {
 	app.Name = "App de linha de comando"
 	app.Usage = "Busca IPs e Dominios acessiveis da internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "golangexample.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "golangexample.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPs endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscaServers,
 		},
 	}
 
@@ -45,5 +53,21 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscaServers(c *cli.Context) {
+	host := c.String("host")
+
+	//net LookupNS
+
+	servers, erro := net.LookupNS(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server)
 	}
 }
